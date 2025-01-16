c4eb541bc2a529c3f6326f8478fafc25db9ae08d
Szymon Wnuk <szwnuk1@gmail.com>
2025-01-16 15:33:52 +0100

    16.01

--- SwiftKata.playground/Contents.swift
+++ SwiftKata.playground/Contents.swift
@@ -684,3 +684,15 @@ func fibonacciIterative(_ n: Int) -> [Int] {
 
     return result
 }
+
+func sumOfDigits4(_ number: Int) -> Int {
+    var result = 0
+    var number  = number
+    while number > 0 {
+
+        result += number % 10
+        number /= 10
+    }
+
+    return result
+}
