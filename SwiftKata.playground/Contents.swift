6be110c989278d56384779dde74d8279005a5df1
Szymon Wnuk <szymon@bereyziat.dev>
2025-04-15 15:45:51 +0200

    15.05

--- SwiftKata.playground/Contents.swift
+++ SwiftKata.playground/Contents.swift
@@ -1816,9 +1816,38 @@ func smallestMissingPositive(_ nums: [Int]) -> Int {
         if !set.contains(i){
             return i
         }
-        i + 1 
+        i + 1
     }
 
 
 }
-smallestMissingPositive([3, 4, -1, 1])
+
+func moveZeroesToEnd(_ array: [Int]) -> [Int]{
+    var result = [Int]()
+    let zeros = array.filter{$0 == 0}
+
+    result = array.filter{$0 != 0} + zeros
+
+    return result
+}
+
+
+func fizzBuzz(_ n: Int) -> [String] {
+
+    var result = [String]()
+
+    for i in  1...n {
+        if i % 3 == 0 && i % 5 == 0 {
+                 result.append("FizzBuzz")
+             } else if i % 3 == 0 {
+                 result.append("Fizz")
+             } else if i % 5 == 0 {
+                 result.append("Buzz")
+             } else {
+                 result.append(String(i))
+             }
+    }
+
+    return result
+}
+fizzBuzz(15)
