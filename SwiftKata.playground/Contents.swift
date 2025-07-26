73dec647f22cebf33905d112a92465850c9e4480
Szymon Wnuk <szymon@bereyziat.dev>
2025-07-27 01:10:54 +0200

    26.07

--- SwiftKata.playground/Contents.swift
+++ SwiftKata.playground/Contents.swift
@@ -2897,4 +2897,24 @@ func mostCommonWordLength(_ sentence: String) -> Int {
     return dict.max{$0.value<$1.value}?.key ?? 0
 }
 
-mostCommonWordLength("hi ok noo")
+func fizzBuzzMapped(_ n: Int) -> [String] {
+    var result = [String]()
+    for i in 1...n {
+        if i % 3 == 0 && i % 5 == 0 {
+            result.append("FizzBuzz")
+        }
+        else if i % 3 == 0 {
+            result.append("Fizz")
+        } else if i % 5 == 0 {
+            result.append("Buzz")
+        } else {
+            result.append(String(i))
+        }
+
+    }
+
+    return result
+}
+
+fizzBuzzMapped(3)
+
